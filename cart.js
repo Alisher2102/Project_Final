a9cdac44f2329402974934c2d67930ccc6d79fb1
welray17 <119338912+welray17@users.noreply.github.com>
2022-12-17 22:01:19 +0600

    Add files via upload

---
+++ cart.js
@@ -0,0 +1,38 @@
+let items = document.getElementsByClassName('item');
+var img = document.createElement("img");
+img.src = "https://cdn3.iconfinder.com/data/icons/shopping-and-ecommerce-28/90/empty_cart-512.png";
+var src = document.querySelector('.items');
+var removeButtons = document.getElementsByClassName('close');
+for (var i=0; i<removeButtons.length; i++){
+    var button = removeButtons[i]
+    button.addEventListener('click', function(event){
+        var clickedButton = event.target;
+        clickedButton.parentElement.parentElement.parentElement.remove();
+        if(items.length==0){
+            document.querySelector('.items').innerHTML = "<h1>Cart is empty</h1>";
+            src.appendChild(img);
+        }
+        updateTotal();
+    });
+};
+function updateTotal(){
+    //var cartItems = document.getElementsByClassName('item');
+    var total = 0;
+    for (var i=0; i<items.length; i++){
+        var item = items[i];
+        var itemPrice = item.getElementsByClassName('price')[0];
+        var itemQuantity = item.getElementsByClassName('quantity')[0];
+        var price = itemPrice.innerText.replace('₸', '');
+        var quantity = itemQuantity.value;
+        console.log(price,quantity)
+        total += price * quantity;
+        
+    }
+    document.getElementsByClassName('subTotal1')[0].innerText = total + '₸';
+    if(items.length!=0)
+        document.getElementsByClassName('finalorder')[0].innerText = total + 5000 + '₸';
+    else document.getElementsByClassName('finalorder')[0].innerText = 0 + '₸'
+}
+
+
+
