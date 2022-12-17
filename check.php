b82d2692da10fc62a9e2060a94bdf801d476eafc
Alisher2102 <93984868+Alisher2102@users.noreply.github.com>
2022-12-17 21:15:48 +0600

    Update check.php

--- check.php
+++ check.php
@@ -3,6 +3,10 @@ $login = filter_var(trim($_POST['Login']), FILTER_SANITIZE_SPECIAL_CHARS);
 $name = filter_var(trim($_POST['Name']), FILTER_SANITIZE_SPECIAL_CHARS);
 $pass = filter_var(trim($_POST['password']), FILTER_SANITIZE_SPECIAL_CHARS);
 $email = filter_var(trim($_POST['Email']), FILTER_SANITIZE_SPECIAL_CHARS);
+if ($_POST["Login"] == "" || $_POST["password"] == "" || $_POST["Name"] == '' || $_POST["Email"] == '') {
+    header("Location: index.php");
+    exit();
+}
 if (mb_strlen($login) < 5 || mb_strlen($login) > 90) {
     echo "Length of the login is prohibitted";
     exit();
@@ -10,6 +14,9 @@ if (mb_strlen($login) < 5 || mb_strlen($login) > 90) {
     echo "Length of the name is prohibitted";
     exit();
 } else if (mb_strlen($pass) < 2 || mb_strlen($pass) > 90) {
+    echo "Length of the password is prohibitted, from 2 to 90 symbols";
+    exit();
+} else if (mb_strlen($email) < 2 || mb_strlen($email) > 90) {
     echo "Length of the password is prohibitted, from 2 to 90 symbols";
     exit();
 }
