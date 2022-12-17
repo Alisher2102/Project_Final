cdd7c3304c0067ea96a634f6f0ec6563fa2edd5f
welray17 <119338912+welray17@users.noreply.github.com>
2022-12-18 01:53:04 +0600

    Update cart.js

--- cart.js
+++ cart.js
@@ -3,6 +3,8 @@ var img = document.createElement("img");
 img.src = "https://cdn3.iconfinder.com/data/icons/shopping-and-ecommerce-28/90/empty_cart-512.png";
 var src = document.querySelector('.items');
 var removeButtons = document.getElementsByClassName('close');
+var quantities = document.getElementsByClassName('quantity');
+updateTotal();
 for (var i=0; i<removeButtons.length; i++){
     var button = removeButtons[i]
     button.addEventListener('click', function(event){
@@ -15,6 +17,10 @@ for (var i=0; i<removeButtons.length; i++){
         updateTotal();
     });
 };
+for (var i=0; i<quantities.length; i++){
+    var quantity =  quantities[i];
+    quantity.addEventListener('change',updateTotal);
+}
 function updateTotal(){
     var total = 0;
     for (var i=0; i<items.length; i++){
@@ -30,6 +36,7 @@ function updateTotal(){
         document.getElementsByClassName('finalorder')[0].innerText = total + 5000 + '₸';
     else document.getElementsByClassName('finalorder')[0].innerText = 0 + '₸'
 }
+
 
 
 
