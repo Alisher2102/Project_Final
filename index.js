7ccf73fc44118c6f419f0cf3d748943b32d251c2
Alisher2102 <93984868+Alisher2102@users.noreply.github.com>
2022-12-19 04:09:25 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
-//Sticky navbar
-window.onscroll=function(){
-    stickyFunc();
-}
-var navbar=document.querySelector(".navbar");
-var sticky=navbar.offsetTop;
-function stickyFunc(){
-    if(window.pageYOffset>=sticky){
-        navbar.classList.add("sticky");
+
+$(document).ready(function() {
+    var previousScroll = 0;
+
+    $(window).scroll(function(){
+       var currentScroll = $(this).scrollTop();
+       if (currentScroll > previousScroll) {
+    	   //Direction: Down
+    	   $(".navbar").slideUp();
+       } else {
+    	   //Direction: Up
+    	   $(".navbar").slideDown();
+       }
+       
+       previousScroll = currentScroll;
+       
+       if (currentScroll == 0) {
+    	   $(".navbar").css("position", "relative");
+       } else {
+    	   $(".navbar").css("position", "fixed");
+       }
+    });
+});
+//sticky footer
+document.addEventListener("DOMContentLoaded", function (event) {
+    var element = document.querySelector('.navbar');
+    var height = element.offsetHeight;
+    if (height < screen.height) {
+        document.querySelector(".footer").classList.add('stikybottom');
     }
-    else{
-        navbar.classList.remove("sticky");
-    }
-}
-//sliding animation
+}, false);
+
+
 var slideIndex=0;
 showSlides();
 function showSlides(){
@@ -31,11 +49,11 @@ function showSlides(){
 //mobile adaptation
 function adapt(){
     var a=document.querySelector(".navbar");
-    if(a.className==="navbar"){
-        a.className+=" responsive";
+    if(a.className==="navbar sticky" || a.className==="navbar"){
+        a.classList.add("responsive");
     }
     else{
-        a.className="navbar";
+        a.classList.remove("responsive");
     }
 }
 //finder function
@@ -72,4 +90,14 @@ function carousel(n){
         slides[i].style.display="none";
     }
     slides[index-1].style.display="flex";
+}
+function showhide(){
+    var take=document.querySelector(".hopa");
+    var check=document.querySelector(".Profile");
+    if(take.style.display==="block" && check.innerHTML==="Profile"){
+        take.style.display="none";
+    }
+    else{
+        take.style.display="block";
+    }
 }
