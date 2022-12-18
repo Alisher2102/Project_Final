30387440d198550b8f2cae77b3962e6c5bd3b838
Alisher2102 <93984868+Alisher2102@users.noreply.github.com>
2022-12-19 04:10:04 +0600

    Update header.php

--- header.php
+++ header.php
@@ -13,6 +13,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="styles.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
 </head>
 
@@ -24,32 +25,33 @@
     ?>
 
         <header>
-            <link rel="stylesheet" href="styles.css">
+            <!-- <link rel="stylesheet" href="styles.css"> -->
+            <div class="header">
 
-            <div class="navbar">
-                <img src="logo-removebg-preview.png" alt="">
-                <div style="display: flex; justify-content: space-between;align-items: center; width: 350px;">
-                    <a href="site.php" class="title">SDU shop</a>
-                    <input type="text" onkeyup="searcher()" class="searcher" placeholder="Search..." name="" id="">
+                <div class="navbar">
+                    <img src="logo-removebg-preview.png" alt="">
+                    <div style="display: flex; justify-content: space-between;align-items: center; width: 350px;">
+                        <a href="site.php" class="title">SDU shop</a>
+                        <input type="text" onkeyup="searcher()" class="searcher" placeholder="Search..." name="" id="">
+                    </div>
+                    <div class="categories">
+                        <a href="">Products</a>
+                        <a href="">Categories</a>
+                    </div>
+                    <div class="prof">
+                        <a href=""><i style="font-size:28px" class="fa">&#xf291;</i> Basket</a>
+                        <a href="exit.php"><i class="fa fa-fw fa-user"></i>Registration</a>
+                        <a href="profile.php"><i class="fa fa-male"></i> Profile</a>
+                    </div>
+                    <a href="javascript:void(0)" class="icon" onclick="adapt()"><i class="fa fa-bars"></i></a>
                 </div>
-                <div class="categories">
-                    <a href="">Products</a>
-                    <a href="">Categories</a>
-                </div>
-                <div class="prof">
-                    <a href=""><i style="font-size:28px" class="fa">&#xf291;</i> Basket</a>
-                    <a href="exit.php"><i class="fa fa-fw fa-user"></i>Registration</a>
-                    <a href="profile.php"><i class="fa fa-male"></i> Profile</a>
-                </div>
-                <a href="javascript:void(0)" class="icon" onclick="adapt()"><i class="fa fa-bars"></i></a>
             </div>
 
-            <script src="index.js"></script>
         </header>
     <?php else : ?>
 
         <header>
-            <link rel="stylesheet" href="styles.css">
+            <!-- <link rel="stylesheet" href="styles.css"> -->
             <div class="navbar">
                 <img src="logo-removebg-preview.png" alt="">
                 <div style="display: flex; justify-content: space-between;align-items: center; width: 350px;">
@@ -67,6 +69,6 @@
                 </div>
                 <a href="javascript:void(0)" class="icon" onclick="adapt()"><i class="fa fa-bars"></i></a>
             </div>
-            <script src="index.js"></script>
+
         </header>
     <?php endif; ?>
