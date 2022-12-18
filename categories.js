21a09a1f972fbd43ff207ee416dac6e0a85d7907
welray17 <119338912+welray17@users.noreply.github.com>
2022-12-18 23:08:33 +0600

    Add files via upload

---
+++ categories.js
@@ -0,0 +1,25 @@
+var cards = document.getElementsByClassName('category');
+for(var i=0; i<cards.length; i++){
+    var card = cards[i];
+    // var name = card.getElementsByClassName('categoryName')[0];
+    card.addEventListener('mouseenter', function(event){
+        var hoveringCard = event.target;
+        var img = hoveringCard.getElementsByTagName('img')[0];
+        var name = hoveringCard.getElementsByClassName('categoryName')[0];
+        console.log(img);
+        img.style.filter = "blur(3px)";
+        name.style.transform = "translateY(-10px)";
+        hoveringCard.style.borderRadius = "20px";
+        img.style.borderRadius = "20px";
+    });
+    card.addEventListener('mouseleave', function(event){
+        var hoveringCard = event.target;
+        var img = hoveringCard.getElementsByTagName('img')[0];
+        var name = hoveringCard.getElementsByClassName('categoryName')[0];
+        console.log(img);
+        img.style.filter = "blur(0px)";
+        name.style.transform = "translateY(10px)";
+        hoveringCard.style.borderRadius = "0px";
+        img.style.borderRadius = "0px";
+    });
+}
