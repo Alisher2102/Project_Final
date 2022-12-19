b6dbcebfddff5df16763b7c08044b05e6b1eecfb
welray17 <119338912+welray17@users.noreply.github.com>
2022-12-19 20:44:24 +0600

    Update cart.php

--- cart.php
+++ cart.php
@@ -1,8 +1,24 @@
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="cartstyles.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital@1&family=Roboto&display=swap" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
 
+  <title>Cart</title>
+</head>
+
+<body>
   <?php
     require("header.php");
   ?>
-<link rel="stylesheet" href="cartstyles.css">
   <div style="display: flex; justify-content: center;">
   <div class="main">
     <div class="cart">
@@ -33,14 +49,13 @@
                   <p class=\"price\">$row[price]₸</p>
               </div>
               <div style=\"text-align: right;align-self: self-start;\">
-                  <button class=\"close\" ><span class=\"material-symbols-outlined\">cancel</span></button>
+                  <button class=\"close material-symbols-outlined\" >cancel</button>
                   <div class=\"quan\">
                   <p style=\"font-size:20px\">Quantity:</p>
                   <input type=\"number\" class=\"quantity\" value=\"1\" min=\"1\" max=\"99\">
                   </div>
               </div>
-          </div>
-          ");
+          </div>");
         }
         ?>
       </div>
