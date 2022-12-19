46a66a54869aa00af01b71240df0a1bbc7a94f3c
Alisher2102 <93984868+Alisher2102@users.noreply.github.com>
2022-12-19 17:12:06 +0600

    Update footer.php

--- footer.php
+++ footer.php
@@ -4,30 +4,33 @@ error_reporting(0);
 if ($_COOKIE['user'] == '') :
 ?>
     <footer>
-        <link rel="stylesheet" href="styles.css">
+        <!-- <link rel="stylesheet" href="styles.css"> -->
 
-        <script src="index.js"></script>
     </footer>
 <?php else : ?>
     <footer>
-        <link rel="stylesheet" href="styles.css">
-        <div class="footer">
-            <div style="display: flex; justify-content: space-between;align-items: center; width: 13rem;">
-                <span class="SPAN">Contact us:</span>
-                <a href="" class="title"><i class="fa fa-telegram" aria-hidden="true"></i></a>
-                <a href="" class="title"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+        <!-- <link rel="stylesheet" href="styles.css"> -->
+        <div class="container">
+
+            <div class="footer">
+                <div style="display: flex; justify-content: space-between;align-items: center; width: 13rem;">
+                    <span class="SPAN">Contact us:</span>
+                    <a href="https://t.me/recker2014brr" class="title"><i class="fa fa-telegram" aria-hidden="true"></i></a>
+                    <a href="https://www.instagram.com/my__page17/" class="title"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                </div>
+                <div class="categories">
+                    <span class="SPAN">Address:</span>
+                    <a href="https://goo.gl/maps/1MHRt9vzPULSJCUz5">1/1 Abylai Khan St. Kaskelen</a>
+                </div>
+                <div class="prof">
+                    <span class="SPAN">® All rights are reserved</span>
+                </div>
             </div>
-            <div class="categories">
-                <span class="SPAN">Address:</span>
-                <a href="https://goo.gl/maps/1MHRt9vzPULSJCUz5">1/1 Abylai Khan St. Kaskelen</a>
-            </div>
-            <div class="prof">
-                <span class="SPAN">® All rights are reserved</span>
-            </div>
-        </div>
-        <script src="index.js"></script>
+
     </footer>
 <?php endif; ?>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+<script src="index.js"></script>
 </body>
 
 </html>
