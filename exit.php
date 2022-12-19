4a76316fe7850e832cf6b93d622ba371725531f6
Alisher2102 <93984868+Alisher2102@users.noreply.github.com>
2022-12-20 00:09:16 +0600

    Update exit.php

--- exit.php
+++ exit.php
@@ -2,5 +2,5 @@
 setcookie('user', $user['Name'], time() - 3600, "/");
 setcookie('user1', $user['Email'], time() - 3600, "/");
 setcookie('user1', $user['Login'], time() - 3600, "/");
-
+setcookie('ID', $user['user_ID'], time() - 3600, "/");
 header("Location: /test/site.php");
